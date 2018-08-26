7cf56e6a394cd26ed0968c3e6bba8b62a866869b
alexandrerisi <alexandrerisi@gmail.com>
2018-08-26 21:53:17 +0200

    logout

---
+++ src/main/java/com/risi/mvc/data/demo/config/CRMSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.risi.mvc.data.demo.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class CRMSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override // Don't use it in production.
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("MANAGER"))
+                .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/index").permitAll()
+                .antMatchers("/customer/**")
+                .hasAnyRole("MANAGER", "ADMIN") //only Manager and Admin are allowed
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login-error");
+    }
+}
